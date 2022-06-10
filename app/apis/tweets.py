e9a884535979fd3fd2a04a40c46ad77a8e94c439
iliasgor <hliasgorantis@gmail.com>
2022-06-10 13:56:17 +0200

    test

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,12 +1,33 @@
 # app/apis/tweets.py
 # pylint: disable=missing-docstring
 
-from flask_restx import Namespace, Resource
+import marshal
+from flask_restx import Namespace, Resource, fields, abort
+from app.models import Tweet
+from app import db
 
-api = Namespace("tweets")
+api = Namespace('tweets')  # Base route
 
+json_tweet = api.model('Tweet',{
+    'id': fields.Integer, 
+    'text': fields.String,
+    'created_at': fields.DateTime
+})
+json_tweet = api.model('Tweet', {
+    'id': fields.Integer(required=True),
+    'text': fields.String(required=True, min_length=1),
+    'author': fields.String(required=True, min_length=1),
+    'created_at': fields.DateTime(required=True),
+})
 
-@api.route("/hello")
+@api.route('/<int:id>')  # route extension (ie: /tweets/<int:id>)
+@api.response(404, 'Tweet not found')
+@api.param('id', 'The tweet unique identifier')
 class TweetResource(Resource):
-    def get(self):
-        return "Goodbye from the 'tweets' namespace!"
+    @api.marshal_with(json_tweet)
+    def get(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            return tweet
--- tests/apis/test_tweet_views.py
+++ tests/apis/test_tweet_views.py
@@ -1,15 +1,30 @@
 # tests/apis/test_tweet_view.py
-from flask_testing import TestCase
-from app import create_app
 
-class TestTweetView(TestCase):
+from flask_testing import TestCase
+from app import create_app, db  # N'oubliez pas d'importer db
+from app.models import Tweet
+
+class TestTweetViews(TestCase):
     def create_app(self):
         app = create_app()
         app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"{app.config['SQLALCHEMY_DATABASE_URI']}_test"
         return app
 
-    def test_tweet(self):
-        response = self.client.get("/tweets/hello")
-        text = response.data.decode()
-        #print(text)
-        self.assertIn("Goodbye", text)
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        
+    def test_tweet_show(self):
+        first_tweet = Tweet(text="First tweet")
+        db.session.add(first_tweet)
+        db.session.commit()
+        response = self.client.get("/tweets/1")
+        response_tweet = response.json
+        print(response_tweet)
+        self.assertEqual(response_tweet["id"], 1)
+        self.assertEqual(response_tweet["text"], "First tweet")
+        self.assertIsNotNone(response_tweet["created_at"])
